2605e5db6e36b97b486a4b00eed1074c51a64563
Vincent Rabaud <vrabaud@willowgarage.com>
2013-01-24 02:14:30 +0100

    update the docs a bit

--- src/rgbd/include/opencv2/rgbd/rgbd.hpp
+++ src/rgbd/include/opencv2/rgbd/rgbd.hpp
@@ -295,7 +295,7 @@ namespace cv
      * @param the normals for every point in the depth image
      * @param mask An image where each pixel is labeled with the plane it belongs to
      *        and 255 if it does not belong to any plane
-     * @param the coefficients of the corresponding planes (a,b,c,d) such that ax+by+cz+d=0
+     * @param the coefficients of the corresponding planes (a,b,c,d) such that ax+by+cz+d=0 and norm(a,b,c)=1
      */
     void
     operator()(const cv::Mat & points3d, const cv::Mat & normals, cv::Mat &mask,
